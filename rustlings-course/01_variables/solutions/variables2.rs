d159cca4048c1e70f5ba14b403d9efc5789bfd3f
J3vrin96 <jeremy.quevrin@hotmail.com>
2026-02-14 12:14:21 +0100

    Complete chapter 01

---
+++ rustlings-course/01_variables/solutions/variables2.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let x = 8;
+
+    if x == 10 {
+        println!("x is ten!");
+    } else {
+        println!("x is not ten!");
+    }
+}
---
+++ rustlings-course/01_variables/solutions/variables3.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let x: i32 = 15;
+
+    println!("Number {x}");
+}
